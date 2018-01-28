c079d27d97f9dfe8c0cd0286d187e860fb3ad147
Filip Godziński <godzinski.filip@gmail.com>
2018-01-28 22:12:55 +0100

    changed return

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
 
+import Search from './Search'
+
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
+    return <Search />
   }
 }
