d8c0317443a589ccc397a85cce50bce7a45066fd
Filip Godziński <godzinski.filip@gmail.com>
2018-01-28 22:15:54 +0100

    propTypes

--- src/containers/University.js
+++ src/containers/University.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class University extends Component {
     render() {
-        
         return (
             <div className="university">
                 <ul>
-                    <li>University: <a href={this.props.universityElem.web_pages[0]}>{this.props.universityElem.name}</a></li>
-                    <li>Country: {this.props.universityElem.country}</li>
-                    <li>Country code: {this.props.universityElem.alpha_two_code}</li>
-                    <li>Domains: 
+                    <li><span>University:</span> <a href={this.props.universityElem.web_pages[0]} target="_blank">{this.props.universityElem.name}</a></li>
+                    <li><span>Country:</span> {this.props.universityElem.country}</li>
+                    <li><span>Country code:</span> {this.props.universityElem.alpha_two_code}</li>
+                    <li>
+                        <span>Domains:</span> 
                         <ul>{this.props.domainsList}</ul>
                     </li>
                 </ul>
@@ -17,3 +18,8 @@ export default class University extends Component {
         )
     }
 }
+
+University.propTypes = {
+    universityElem: PropTypes.object,
+    domainsList: PropTypes.array
+};
