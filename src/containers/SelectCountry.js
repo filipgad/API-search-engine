0512b00b7cb0f6d3a2ffe384abdffc8152f70d23
Filip Godziński <godzinski.filip@gmail.com>
2018-01-28 22:15:11 +0100

    added comments, propTypes

--- src/containers/SelectCountry.js
+++ src/containers/SelectCountry.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 export default class SelectCountry extends Component {
     render() {
@@ -6,24 +7,25 @@ export default class SelectCountry extends Component {
         let country = this.props.universityResults.map( universityElem => {
             return universityElem.country
         }).forEach ( countryElem => {
-            if (countryArr.indexOf(countryElem) < 0) {
-            countryArr.push(countryElem);
+            if (countryArr.indexOf(countryElem) < 0) { /* checking the repeatability of elements */
+                countryArr.push(countryElem);
             }
         });
-        countryArr.sort();
+        countryArr.sort(); /* sort list of countries */
         let countries = countryArr.map( (countryArrElem, countryArrIndex) => {
-            return (
-            <option key={countryArrIndex+1} value={countryArrElem}>{countryArrElem}</option>
-            )
+            return <option key={countryArrIndex+1} value={countryArrElem}>{countryArrElem}</option>;
         });
         return (
-            <label>
-                Select country:
-                <select value={this.props.selected} onChange={this.props.handleCountryChange}>
-                    <option key="0" value="all">All</option>
-                    {countries}
-                </select>
-            </label>
+            <select value={this.props.selected} onChange={this.props.handleCountryChange} className="select_container">
+                <option key="0" value="all">All countries</option>
+                {countries}
+            </select>
         )
     }
+}
+
+SelectCountry.propTypes = {
+    handleCountryChange: PropTypes.func,
+    universityResults: PropTypes.array,
+    selected: PropTypes.string
 }
